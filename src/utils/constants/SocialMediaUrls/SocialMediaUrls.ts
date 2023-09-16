74ed813d33603c7d9b5478efd1805ed3beddf82b
Brijesh Kumar <bk10895@gmail.com>
2023-09-16 14:10:20 +0530

    chore(update): update linkedin url

--- src/utils/constants/SocialMediaUrls/SocialMediaUrls.ts
+++ src/utils/constants/SocialMediaUrls/SocialMediaUrls.ts
@@ -1,5 +1,5 @@
 export const GITHUB_URL = "https://github.com/brij1728";
 export const TWITTER_X_URL = "https://twitter.com/bk10895";
 export const LINKEDIN_URL =
-  "https://www.linkedin.com/in/brijesh-kumar-93893497/";
+  "https://www.linkedin.com/in/brijeshapp/";
 export const EMAIL_URL = "mailto: bk10895@gmail.com";
